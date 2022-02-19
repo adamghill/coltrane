d9a9f6b4c06b3a61c72e52edb6c5409b0208581d
adamghill <adamghill@yahoo.com>
2022-02-19 14:16:17 -0500

    Support passing in a specific path from the template for `directory_contents`. Better handling of content directory.

--- coltrane/templatetags/coltrane_tags.py
+++ coltrane/templatetags/coltrane_tags.py
@@ -1,9 +1,10 @@
 from typing import Dict
 
 from django import template
+from django.utils.safestring import SafeString
 
 from coltrane.renderer import get_html_and_markdown
-from coltrane.retriever import get_content_paths
+from coltrane.retriever import get_content_directory, get_content_paths
 
 
 register = template.Library()
@@ -17,6 +18,9 @@ def directory_contents(context, directory: str = None) -> Dict[str, str]:
 
         if directory.startswith("/"):
             directory = directory[1:]
+    elif isinstance(directory, SafeString):
+        # Force SafeString to be a normal string so it can be used with `Path` later
+        directory = directory + ""
 
     content_paths = get_content_paths(directory)
     contents = []
@@ -24,11 +28,17 @@ def directory_contents(context, directory: str = None) -> Dict[str, str]:
     for path in content_paths:
         if path.name != "index.md":
             path_slug = path.name[:-3]
-
             content_slug = path_slug
+            content_directory = get_content_directory()
 
             if directory:
                 content_slug = f"{directory}/{path_slug}"
+                content_directory = content_directory / directory
+
+            directory_without_name = str(path).replace(path.name, "")[:-1]
+
+            if str(content_directory) != directory_without_name:
+                continue
 
             (_, metadata) = get_html_and_markdown(content_slug)
             title = metadata.get("title")
